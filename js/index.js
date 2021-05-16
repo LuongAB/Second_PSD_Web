3b72dbd627f2c98346a1a8f49ee5589ec4d6334a
Luong <LuongTran1421@gmail.com>
2021-05-16 11:59:21 +0700

    Done UI

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(".switch label").on("click", function () {
+	var indicator = $(this).parent(".switch").find("span");
+	if ($(this).hasClass("right")) {
+		$(indicator).addClass("right");
+	} else {
+		$(indicator).removeClass("right");
+	}
+});
